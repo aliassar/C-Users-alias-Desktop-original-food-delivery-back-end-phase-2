5e26d53680999f047b651b2038222d4a738bf86b
Ali Assar <ali.assar8@gmail.com>
2020-05-04 18:47:06 +0430

    Fix not running problem
    make intellij recognize pom.xml

--- src/main/java/IE/filter/LoggingFilter.java
+++ src/main/java/IE/filter/LoggingFilter.java
@@ -18,7 +18,7 @@ public class LoggingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        System.out.println("hi");
+        System.out.println("wwww");
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String token =  request.getParameter("token");
